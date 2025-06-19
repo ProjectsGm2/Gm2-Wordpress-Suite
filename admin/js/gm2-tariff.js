10fd1f8465d38f731ae0f4c5c3b2db44ce88a624
ProjectsGm2 <projects.gm2web@gmail.com>
2025-06-19 06:38:06 +0530

    Display tariff form errors

--- admin/js/gm2-tariff.js
+++ admin/js/gm2-tariff.js
@@ -1,5 +1,16 @@
 jQuery(function($){
-    $('#gm2-add-tariff-form').on('submit', function(e){
+    var $form = $('#gm2-add-tariff-form');
+    if (!$form.length) {
+        return;
+    }
+
+    var $error = $('#gm2-tariff-error');
+    if (!$error.length) {
+        $error = $('<div id="gm2-tariff-error" class="notice notice-error" style="display:none;"></div>');
+        $form.before($error);
+    }
+
+    $form.on('submit', function(e){
         e.preventDefault();
         var data = {
             action: 'gm2_add_tariff',
@@ -9,7 +20,7 @@ jQuery(function($){
             tariff_status: $('#tariff_status').is(':checked') ? 'enabled' : 'disabled'
         };
         $.post(gm2Tariff.ajax_url, data, function(response){
-            if(response.success){
+            if (response.success) {
                 var t = response.data;
                 var row = '<tr>'+
                     '<td>'+t.name+'</td>'+
@@ -18,10 +29,11 @@ jQuery(function($){
                     '<td><a href="'+t.edit_url+'">View</a> | <a href="'+t.edit_url+'">Edit</a> | <a href="'+t.delete_url+'" onclick="return confirm(\'Are you sure?\');">Delete</a></td>'+
                 '</tr>';
                 $('#gm2-tariff-table tbody').append(row);
-                $('#gm2-add-tariff-form')[0].reset();
+                $form[0].reset();
+                $error.hide().text('');
             } else {
                 var msg = response.data && response.data.message ? response.data.message : response.data;
-                alert(msg || 'Error');
+                $error.text(msg || 'Error').show();
             }
         });
     });
