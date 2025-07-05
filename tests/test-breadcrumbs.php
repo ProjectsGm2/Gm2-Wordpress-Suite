86f47b57a7db026a5d58c3341c811c747b1a5dd2
ProjectsGm2 <projects.gm2web@gmail.com>
2025-07-05 07:29:32 +0530

    Add breadcrumbs shortcode test and docs

--- tests/test-breadcrumbs.php
+++ tests/test-breadcrumbs.php
@@ -18,4 +18,26 @@ class BreadcrumbsTest extends WP_UnitTestCase {
         $seo->run();
         $this->assertNotFalse( has_action('wp_footer', [$seo, 'output_breadcrumbs']) );
     }
+
+    public function test_shortcode_outputs_ordered_list_with_json_ld() {
+        $post_id = self::factory()->post->create([
+            'post_title'   => 'Sample',
+            'post_content' => 'Content',
+        ]);
+        $seo = new Gm2_SEO_Public();
+        $this->go_to(get_permalink($post_id));
+        setup_postdata(get_post($post_id));
+
+        $output = $seo->gm2_breadcrumbs_shortcode();
+
+        $this->assertStringContainsString('<ol>', $output);
+        $this->assertStringContainsString('</ol>', $output);
+        $this->assertStringContainsString('<script type="application/ld+json">', $output);
+
+        preg_match('/<script type="application\/ld\+json">(.*?)<\/script>/', $output, $m);
+        $json = $m[1] ?? '';
+        $data = json_decode($json, true);
+        $this->assertIsArray($data);
+        $this->assertSame('BreadcrumbList', $data['@type']);
+    }
 }
