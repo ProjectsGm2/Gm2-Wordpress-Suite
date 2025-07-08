0ec9b08967f7d96a108dfb95660a19ea8ef439c2
ProjectsGm2 <projects.gm2web@gmail.com>
2025-07-08 23:37:44 +0530

    Update autoloader to search plugin directories

--- includes/autoload.php
+++ includes/autoload.php
@@ -3,10 +3,13 @@ spl_autoload_register(function ($class) {
     if (strpos($class, 'Gm2\\') !== 0) {
         return;
     }
-    $name = substr($class, 4);                  // strip namespace prefix
-    $name = str_replace('_', DIRECTORY_SEPARATOR, $name);
-    $file = GM2_PLUGIN_DIR . $name . '.php';
-    if (file_exists($file)) {
-        require_once $file;
+
+    $name = substr($class, 4); // class name without namespace
+    foreach (['includes', 'admin', 'public'] as $dir) {
+        $file = GM2_PLUGIN_DIR . $dir . '/' . $name . '.php';
+        if (file_exists($file)) {
+            require_once $file;
+            return;
+        }
     }
 });
