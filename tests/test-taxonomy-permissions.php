f4568b2bb344571be4fbe09ad4565e248ed772a1
ProjectsGm2 <projects.gm2web@gmail.com>
2025-07-10 21:45:23 +0530

    Add taxonomy description rule

--- tests/test-taxonomy-permissions.php
+++ tests/test-taxonomy-permissions.php
@@ -21,3 +21,27 @@ class TaxonomyPermissionsTest extends WP_UnitTestCase {
     }
 }
 
+class TaxonomyContentRulesTest extends WP_Ajax_UnitTestCase {
+    private function run_check($content) {
+        $this->_setRole('administrator');
+        $_POST['taxonomy'] = 'category';
+        $_POST['content'] = $content;
+        $_POST['title'] = 'Test';
+        $_POST['description'] = 'Desc';
+        $_POST['_ajax_nonce'] = wp_create_nonce('gm2_check_rules');
+        $_REQUEST['_ajax_nonce'] = $_POST['_ajax_nonce'];
+        try { $this->_handleAjax('gm2_check_rules'); } catch (WPAjaxDieContinueException $e) {}
+        return json_decode($this->_last_response, true);
+    }
+
+    public function test_description_word_count_rule() {
+        update_option('gm2_content_rules', ['tax_category' => 'Description has at least 150 words']);
+        $resp = $this->run_check(str_repeat('word ', 160));
+        $this->assertTrue($resp['success']);
+        $this->assertTrue($resp['data']['description-has-at-least-150-words']);
+
+        $resp = $this->run_check(str_repeat('word ', 10));
+        $this->assertFalse($resp['data']['description-has-at-least-150-words']);
+    }
+}
+
