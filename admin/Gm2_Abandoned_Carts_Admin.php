0f8f23fe8ce95505d2a4fe5deef5ea17bd74b387
ProjectsGm2 <projects.gm2web@gmail.com>
2025-08-06 04:02:27 +0530

    Remove abandoned cart timeout setting and update docs

--- admin/Gm2_Abandoned_Carts_Admin.php
+++ admin/Gm2_Abandoned_Carts_Admin.php
@@ -8,7 +8,6 @@ if (!defined('ABSPATH')) {
 class Gm2_Abandoned_Carts_Admin {
     public function run() {
         add_action('admin_menu', [ $this, 'add_menu' ]);
-        add_action('admin_post_gm2_ac_settings', [ $this, 'save_settings' ]);
     }
 
     public function add_menu() {
@@ -22,28 +21,8 @@ class Gm2_Abandoned_Carts_Admin {
         );
     }
 
-    public function save_settings() {
-        check_admin_referer('gm2_ac_settings');
-        update_option('gm2_ac_timeout', absint($_POST['gm2_ac_timeout']));
-        wp_redirect(admin_url('admin.php?page=gm2-abandoned-carts&updated=1'));
-        exit;
-    }
-
     public function display_page() {
-        $timeout = get_option('gm2_ac_timeout', 60);
         echo '<div class="wrap"><h1>' . esc_html__('Abandoned Carts', 'gm2-wordpress-suite') . '</h1>';
-        if (isset($_GET['updated'])) {
-            echo '<div class="updated notice"><p>' . esc_html__('Settings saved.', 'gm2-wordpress-suite') . '</p></div>';
-        }
-        echo '<form method="post" action="' . esc_url(admin_url('admin-post.php')) . '">';
-        wp_nonce_field('gm2_ac_settings');
-        echo '<input type="hidden" name="action" value="gm2_ac_settings">';
-        echo '<table class="form-table"><tbody>';
-        echo '<tr><th scope="row"><label for="gm2_ac_timeout">' . esc_html__('Abandonment timeout (minutes)', 'gm2-wordpress-suite') . '</label></th>';
-        echo '<td><input name="gm2_ac_timeout" id="gm2_ac_timeout" type="number" value="' . esc_attr($timeout) . '" class="small-text"></td></tr>';
-        echo '</tbody></table>';
-        submit_button();
-        echo '</form>';
 
         $table = new GM2_AC_Table();
         $table->prepare_items();
