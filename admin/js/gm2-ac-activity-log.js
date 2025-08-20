4b6e25268f1fccc79b206a72e0b702b277addae2
ProjectsGm2 <projects.gm2web@gmail.com>
2025-08-20 06:01:40 +0530

    Test activity log rendering

--- admin/js/gm2-ac-activity-log.js
+++ admin/js/gm2-ac-activity-log.js
@@ -21,13 +21,23 @@ jQuery(function($){
         }).done(function(response){
             var colspan = tr.children().length;
             var html = '<tr class="gm2-ac-activity-row"><td colspan="'+colspan+'">';
-            if(response.success && response.data.length){
+            var hasActivity = response.success && response.data && response.data.activity && response.data.activity.length;
+            var hasVisits = response.success && response.data && response.data.visits && response.data.visits.length;
+            if(hasActivity){
                 html += '<ul class="gm2-ac-activity-log">';
-                response.data.forEach(function(item){
+                response.data.activity.forEach(function(item){
                     html += '<li><strong>'+item.changed_at+'</strong> '+item.action+' '+item.sku+' x'+item.quantity+'</li>';
                 });
                 html += '</ul>';
-            } else {
+            }
+            if(hasVisits){
+                html += '<ul class="gm2-ac-activity-log gm2-ac-visit-log">';
+                response.data.visits.forEach(function(visit){
+                    html += '<li><strong>'+visit.visit_start+'</strong> '+visit.entry_url+' \u2192 '+visit.exit_url+'</li>';
+                });
+                html += '</ul>';
+            }
+            if(!hasActivity && !hasVisits){
                 html += '<em>'+gm2AcActivityLog.empty+'</em>';
             }
             html += '</td></tr>';
