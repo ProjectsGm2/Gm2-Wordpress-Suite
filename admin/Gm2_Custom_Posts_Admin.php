b055bfab3fc2f21a1d946bdf21beb7a4e29df953
ProjectsGm2 <projects.gm2web@gmail.com>
2025-08-26 17:19:47 +0530

    Ensure nonce field renders in bulk post delete form

--- admin/Gm2_Custom_Posts_Admin.php
+++ admin/Gm2_Custom_Posts_Admin.php
@@ -1061,7 +1061,7 @@ class Gm2_Custom_Posts_Admin {
         $table->prepare_items();
         echo '<form method="post" action="' . esc_url(admin_url('admin-post.php')) . '">';
         echo '<input type="hidden" name="action" value="gm2_delete_post_type" />';
-        wp_nonce_field( 'bulk-posts', '_wpnonce', true, false );
+        wp_nonce_field( 'bulk-posts' );
         $table->display();
         echo '</form>';
 
