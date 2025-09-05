daf7e2d7808da500216f380965a476e61a7cc2d6
ProjectsGm2 <projects.gm2web@gmail.com>
2025-09-05 22:55:22 +0530

    Use chunk helper for longtask observer

--- assets/js/perf/longtask.js
+++ assets/js/perf/longtask.js
@@ -3,16 +3,18 @@
  *
  * @return {void}
  */
+import { chunk } from './yield.js';
 export function init() {
     if (typeof PerformanceObserver === 'undefined') {
         return;
     }
     try {
         const observer = new PerformanceObserver((list) => {
-            for (const entry of list.getEntries()) {
+            const entries = list.getEntries();
+            chunk(entries, 50, (entry) => {
                 // eslint-disable-next-line no-console
                 console.log('Long task:', entry);
-            }
+            });
         });
         observer.observe({ type: 'longtask', buffered: true });
     } catch (e) {
